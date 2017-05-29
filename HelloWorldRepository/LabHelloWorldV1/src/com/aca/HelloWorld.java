0e5bac8aa8e70b23d471f4a2bc5a5389aeee8c0f
grantdanielllc <d_l_lazenby@yahoo.com>
2017-05-29 18:21:19 -0500

    initial commit

---
+++ HelloWorldRepository/LabHelloWorldV1/src/com/aca/HelloWorld.java
@@ -0,0 +1,13 @@
+package com.aca;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		
+		Person person = new Person();
+		
+		System.out.println("Hello " + person.myName);
+
+	}
+
+}
---
+++ HelloWorldRepository/LabHelloWorldV1/src/com/aca/Person.java
@@ -0,0 +1,14 @@
+package com.aca;
+
+/* 1. change myName to your name and re-test Person.
+ * 
+ * 2. add 2 more instance variables - city and state.
+ * 
+ * 3. write a method that returns the myName, city, and state as a nicely concatenated String.
+ * 
+ * 4. update HelloWorld to make 2 Person objects and re-test Person.
+*/
+public class Person {
+
+	public String myName = "Daniel Lazenby";
+}
