cb7629f0dc29febe9a446d36dafbc7bac2a5c7c4
Andrew White <Andrew White@161.31.176.72>
2017-06-03 14:29:29 -0500

    add 2nd person object

--- HelloWorldRepository/LabHelloWorldV1/src/com/aca/HelloWorld.java
+++ HelloWorldRepository/LabHelloWorldV1/src/com/aca/HelloWorld.java
@@ -5,10 +5,12 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		
 		Person person = new Person();
+		Person person2 = new Person();
 		
 		System.out.println("Hello " + person.myName);
 		
 		person.myName = "David Lazenby";
+		person2.myName = "Andrew White";
 		
 		System.out.println("Hello " + person.myName);
 
