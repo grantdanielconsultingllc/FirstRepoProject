6c47cae02628ed95b8ec30bcad3289189f345fea
whiteandrewj <white_andrewj@yahoo.com>
2017-06-07 20:25:50 -0500

    edit println

--- HelloWorldRepository/LabHelloWorldV1/src/com/aca/Andrew.java
+++ HelloWorldRepository/LabHelloWorldV1/src/com/aca/Andrew.java
@@ -3,7 +3,7 @@ package com.aca;
 public class Andrew {
 
 	public static void main(String[] args) {
-		System.out.println(" my name is Andrew the Abstraction King!");
+		System.out.println(" my name is Andrew the Abstraction King! Yeah, you know it.");
 
 	}
 
